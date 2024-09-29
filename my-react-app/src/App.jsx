49903f1bd2ba630a83dea86d27f7d474096ad673
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-09-29 21:39:50 +0530

    Introduction to props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,12 +3,18 @@ import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
 import Button from "./Button/Button.jsx";
+import Student from "./Student.jsx";
 
 
 function App() {
   
   return(
-    <Button/>
+    <>
+    <Student name="Kevin Andel Josy" age={23} isStudent={false}/>
+    <Student name="Alen Abel Josy" age={13} isStudent={true}/>
+    <Student name="Reena Josy" age={47} isStudent={false}/>
+    <Student name="Josy C.A." age={57} isStudent={false}/>
+    </>
   );
 }
 
