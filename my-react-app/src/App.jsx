aa7d77a27538573d378456d9f930cf8df8a7ea79
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-09-29 20:14:46 +0530

    Learned modular styling

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,17 +2,13 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
+import Button from "./Button/Button.jsx";
 
 
 function App() {
   
   return(
-    <>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-    </>
+    <Button/>
   );
 }
 
