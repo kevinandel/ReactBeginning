80ee5af9d0922f157519f041f5e0c742998c02ac
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-09-29 19:52:01 +0530

    Created cards and added styles to it

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,19 @@
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
+import Card from "./Card.jsx";
+
 
 function App() {
   
   return(
     <>
-      <Header/>
-      <Food/>
-      <Footer/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+
+  return(
+    <div className="card">
+      <img className = "card-image" src="https://via.placeholder.com/150" alt="profile picture" />
+      <h2 className="card-title">Kevin Andel Josy</h2>
+      <p className="card-text">Learning MERN stack development and take photos</p>
+    </div>
+  );
+}
+
+export default Card;
