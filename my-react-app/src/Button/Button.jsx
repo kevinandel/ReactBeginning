6ab7c7c77f4b03e9add5e58695686ebb7727fe04
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-09-29 21:09:46 +0530

    Learned inline styling

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -1,9 +1,35 @@
+/*
 import styles from './Button.module.css'
 
 function Button() {
 
   return(
       <button className={styles.button}>Click me</button>
+  );
+}
+
+export default Button;
+*/
+
+
+
+// Learning inline styling. Used for minimal styling like buttons
+
+import styles from './Button.module.css'
+
+function Button() {
+
+  const styles = {
+      backgroundColor: "hsl(200, 100%, 50%)",
+      color: "white",
+      padding: "10px 20px",
+      borderRadius: "5px",
+      border: "none",
+      cursor: "pointer",
+  }
+
+  return(
+      <button style={styles}>Click me</button>
   );
 }
 
