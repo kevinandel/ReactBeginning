01f1d12e350af8720c1b9fd0c7eb32be3e53b979
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-01 19:06:24 +0530

    Learned onChange event handler and applied it to various input elements

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -3,6 +3,74 @@ import React, {useState} from 'react';
 function MyComponent() {
 
   const [name, setName] = useState("Guest");
+  const [quantity, setQuantity] = useState(1);
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleQuantityChange(event) {
+    setQuantity(event.target.value);
+  }
+ 
+  function handleCommentChange(event) {
+    setComment(event.target.value);
+  }
+
+  function handlePaymentChange(event) {
+    setPayment(event.target.value);
+  }
+
+  function handleShippingChange(event) {
+    setShipping(event.target.value);
+  }
+
+  return(<div>
+
+          <input value={name} onChange={handleNameChange}/>
+          <p>Name: {name}</p>
+
+          <input value={quantity} onChange={handleQuantityChange} type="number"/>
+          <p>Quantity: {quantity}</p>
+
+          <textarea value={comment} onChange={handleCommentChange} placeholder="Add delivery instructions"></textarea>
+          <p>Comment: {comment}</p>
+
+          <select value={payment} onChange={handlePaymentChange}>
+            <option value="">Select an option</option>
+            <option value="Visa">Visa</option>
+            <option value="MasterCard">MasterCard</option>
+            <option value="GiftCard">GiftCard</option>
+          </select>
+          <p>Payment: {payment}</p>
+
+          <label>
+            <input type="radio" value="Pick up" checked={shipping === "Pick up"} onChange={handleShippingChange}/>
+            Pick up
+          </label><br />
+          <label>
+            <input type="radio" value="Delivery" checked={shipping === "Delivery"} onChange={handleShippingChange}/>
+            Delivery
+          </label>
+          <p>Shipping: {shipping}</p>
+          
+        </div>);
+}
+
+export default MyComponent;
+
+
+
+
+/*
+import React, {useState} from 'react';
+
+function MyComponent() {
+
+  const [name, setName] = useState("Guest");
   const [age, setAge] = useState(0);
   const [isEmployed, setIsEmployed] = useState(false);
 
@@ -31,3 +99,5 @@ function MyComponent() {
 }
 
 export default MyComponent;
+
+*/
