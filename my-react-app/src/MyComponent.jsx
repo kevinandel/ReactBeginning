d76a0b27b18ba632e5f611be0482135158f51e63
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-02 16:31:55 +0530

    Learned how to use useEffect() hook

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,3 +1,52 @@
+import React, {useState, useEffect} from 'react';
+
+function MyComponent() {
+
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
+
+  // useEffect(() => {
+  //   document.title = `Count: ${count}`
+  // }); //runs the function every time the page re-renders
+
+  // useEffect(() => {
+  //   document.title = `Count: ${count}`
+  // }, []); //runs the function only once during mount when we add an empty array of dependencies
+
+  useEffect(() => {
+    document.title = `Count: ${count} ${color}`
+  }, [count, color]); //runs the function every time the value of the count inside dependency array changes and during mount
+
+  function addCount() {
+    setCount(c =>  c + 1);
+
+  }
+
+  function subtractCount() {
+    setCount(c =>  c - 1);
+
+  }
+  
+  function changeColor() {
+    setColor(c => c === "green" ? "red" : "green");
+  }
+
+  return(<>
+          <p style={{color: color}}>Count: {count}</p>
+          <button onClick={addCount}>Add</button>
+          <button onClick={subtractCount}>Subtract</button><br />
+          <button onClick={changeColor}>Change Color</button>
+        </>);
+}
+
+
+export default MyComponent;
+
+
+
+
+
+/*
 import React, {useState} from 'react';
 
 function MyComponent() {
@@ -55,6 +104,7 @@ function MyComponent() {
 
 export default MyComponent;
 
+*/
 
 
 
