a004713bb6a203819ba241fff489927e6ae0b50b
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-02 10:50:30 +0530

    Learned how to create an array of objects and update their states

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,6 +2,69 @@ import React, {useState} from 'react';
 
 function MyComponent() {
 
+  const [cars, setCars] = useState([]);
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carMake, setCarMake] = useState("");
+  const [carModel, setCarModel] = useState("");
+
+
+  function handleAddCar() {
+
+    const newCar = {year: carYear, make: carMake, model: carModel};
+
+    setCars(prevCars => [...prevCars, newCar]);
+
+    setCarYear(new Date().getFullYear());
+    setCarMake("");
+    setCarModel("");
+  }
+
+  function handleRemoveCar(index) {
+    setCars(prevCars => prevCars.filter((_, i) => i !== index));
+  }
+
+  function handleYearChange(event) {
+    setCarYear(event.target.value);
+  }
+
+  function handleMakeChange(event) {
+    setCarMake(event.target.value)
+  }
+
+  function handleModelChange(event) {
+    setCarModel(event.target.value)
+  }
+
+
+  return(<div>
+          <h2>List of Car Objects</h2>
+          <ul>
+            {cars.map((car, index) => 
+            <li key={index} onClick={() => handleRemoveCar(index)}>
+              {car.year} {car.make} {car.model}
+            </li>)}
+          </ul>
+
+          <input type="number" value={carYear} onChange={(event) => handleYearChange(event)}/><br />
+          <input type="text" value={carMake} onChange={(event) => handleMakeChange(event)} placeholder="Enter car make"/><br />
+          <input type="text" value={carModel} onChange={(event) => handleModelChange(event)} placeholder="Enter car model"/><br />
+          <button onClick={handleAddCar}>Add Car</button>
+        </div>);
+}
+
+
+export default MyComponent;
+
+
+
+
+
+/*
+
+import React, {useState} from 'react';
+
+function MyComponent() {
+
   const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
   function handleAddFood() {
@@ -29,6 +92,8 @@ function MyComponent() {
 }
 
 export default MyComponent;
+
+*/
 
 
 
